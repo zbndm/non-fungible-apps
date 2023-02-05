a197b50bf265cd46bb46aca0232d3bd4891d1688
dmytro zybin <zbndmt@gmail.com>
2023-02-05 08:22:00 +0000

    first commit

---
+++ ui/src/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from './theme';
+import { Provider as ReduxProvider } from 'react-redux';
+import { store } from './store';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <ReduxProvider store={store}>
+      <ChakraProvider theme={theme} resetCSS>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </ChakraProvider>
+    </ReduxProvider>
+  </React.StrictMode>
+);
+
